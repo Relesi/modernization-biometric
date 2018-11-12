5f3ed1786d9ab4240abd121b7ea3dc6eb388e591
Renato Lessa da Silva <renatolessa.2011@hotmail.com>
2018-11-12 13:10:12 -0200

    creating initial project structure

---
+++ src/main/java/com/relesi/modernizationbiometric/api/ModernizationBiometricApplication.java
@@ -0,0 +1,12 @@
+package com.relesi.modernizationbiometric.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernizationBiometricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernizationBiometricApplication.class, args);
+	}
+}
