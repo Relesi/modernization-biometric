53e54a3eee3a9d046c937bbf95182ab828cf792c
Renato Lessa da Silva <renatolessa.2011@hotmail.com>
2018-11-20 20:29:37 -0200

    creating JPA entities and versioning the database with flyway

--- src/main/java/com/relesi/modernizationbiometric/api/ModernizationBiometricApplication.java
+++ src/main/java/com/relesi/modernizationbiometric/api/ModernizationBiometricApplication.java
@@ -1,4 +1,4 @@
-package com.relesi.modernizationbiometric.api;
+ package com.relesi.modernizationbiometric.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/relesi/modernizationbiometric/api/enums/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.relesi.modernizationbiometric.api.enums;
+
+public enum PerfilEnum {
+	
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+
+}
