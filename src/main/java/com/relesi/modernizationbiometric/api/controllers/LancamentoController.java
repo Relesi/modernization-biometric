dba5b9624585cde1d6ac95d2d71601ccfa0a6de6
Renato Lessa da Silva <renatolessa.2011@hotmail.com>
2018-11-29 14:12:02 -0200

    implementation of JWT tokens authorization

--- src/main/java/com/relesi/modernizationbiometric/api/controllers/LancamentoController.java
+++ src/main/java/com/relesi/modernizationbiometric/api/controllers/LancamentoController.java
@@ -177,7 +177,7 @@ public class LancamentoController {
 	 */
 
 	@DeleteMapping(value = "/{id}")
-	@PreAuthorize("hasAnyRole('ADMIN)")
+	@PreAuthorize("hasAnyRole('ADMIN')")	
 	public ResponseEntity<Response<String>> remover(@PathVariable("id") Long id) {
 		log.info("Removendo lançamento: {}", id);
 		Response<String> response = new Response<String>();
---
+++ src/main/java/com/relesi/modernizationbiometric/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.relesi.modernizationbiometric.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.relesi.modernizationbiometric.api.entities.Funcionario;
+import com.relesi.modernizationbiometric.api.security.JwtUserFactory;
+import com.relesi.modernizationbiometric.api.services.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
